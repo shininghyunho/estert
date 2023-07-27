0c1ebcec736aa51123333bfff031d23cee0be810
shininghyunho <gusgh3315@uos.ac.kr>
2023-07-27 17:40:08 +0900

    fix : object mockking 해제

--- src/test/kotlin/estert/domain/api/predict/PredictHandlerTest.kt
+++ src/test/kotlin/estert/domain/api/predict/PredictHandlerTest.kt
@@ -5,35 +5,47 @@ import estert.common.handler.HttpHandler
 import estert.domain.api.predict.dto.PredictRequest
 import estert.domain.api.predict.dto.PredictResponse
 import estert.dummy.DummyEntity
+import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.test.TestCase
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkObject
+import io.mockk.unmockkObject
+import kotlinx.coroutines.test.TestResult
 
-class PredictHandlerTest : BehaviorSpec({
-    mockkObject(HttpHandler) // object 클래스 mocking
-    val gson = mockk<Gson>()
+class PredictHandlerTest : BehaviorSpec() {
+    init {
+        mockkObject(HttpHandler) // object 클래스 mocking
 
-    val predictHandler = PredictHandler(gson)
+        // unmockObject
+        afterSpec {
+            unmockkObject(HttpHandler)
+        }
 
-    Given("예측 요청시") {
-        val request = PredictRequest(
-            time = 1,
-            latitude = 36.5,
-            longitude = 127.5,
-        )
-        When("정상 요청을 하면") {
-            every { HttpHandler.get(any()) } returns ""
-            every { gson.fromJson("", any<Class<Array<PredictResponse.PredictHouse>>>()) } returns DummyEntity.predictHouseList.toTypedArray()
-            val response = predictHandler.predict(request)
-            Then("예측 결과를 반환한다") {
-                response.predictHouseList.size shouldBe DummyEntity.predictHouseList.size
-                // print all
-                response.predictHouseList.forEach {
-                    println("id: ${it.id}, time: ${it.time}")
+        val gson = mockk<Gson>()
+
+        val predictHandler = PredictHandler(gson)
+
+        Given("예측 요청시") {
+            val request = PredictRequest(
+                time = 1,
+                latitude = 36.5,
+                longitude = 127.5,
+            )
+            When("정상 요청을 하면") {
+                every { HttpHandler.get(any()) } returns ""
+                every { gson.fromJson("", any<Class<Array<PredictResponse.PredictHouse>>>()) } returns DummyEntity.predictHouseList.toTypedArray()
+                val response = predictHandler.predict(request)
+                Then("예측 결과를 반환한다") {
+                    response.predictHouseList.size shouldBe DummyEntity.predictHouseList.size
+                    // print all
+                    response.predictHouseList.forEach {
+                        println("id: ${it.id}, time: ${it.time}")
+                    }
                 }
             }
         }
     }
-})
+}
